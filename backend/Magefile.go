94f3c897a5a6037bf3168459e7c3a8bf417d4fe7
Will Thomson <will.tho@outlook.com>
2020-11-12 10:58:17 +1300

    Add build comments

--- backend/Magefile.go
+++ backend/Magefile.go
@@ -9,11 +9,14 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
+// Callback for setting CGO_ENABLED for building on windows.
+// See https://github.com/grafana/grafana-plugin-sdk-go/blob/002de0b2b925ac930952dc380682100dee27b439/build/common.go#L108.
 var cb build.BeforeBuildCallback = func(cfg build.Config) (build.Config, error) {
 	cfg.EnableCGo = true
 	return cfg, nil
 }
 
+// Main entry-point for building plugin.
 func BuildPlugin() {
 	if runtime.GOOS == "windows" {
 		var err = build.SetBeforeBuildCallback(cb)
