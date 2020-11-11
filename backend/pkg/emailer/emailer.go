65407f1a18bf203ed8f3e9a5ad1d3db3c42d915b
josh <griffinjoshua5@gmail.com>
2020-11-12 09:00:30 +1300

    Add emailer error handling

--- backend/pkg/emailer/emailer.go
+++ backend/pkg/emailer/emailer.go
@@ -17,24 +17,28 @@ func New(config *auth.EmailConfig) *Emailer {
 	return &Emailer{email: config.Email, password: config.Password, host: config.Host, port: config.Port}
 }
 
-func (e *Emailer) CreateAndSend(attachmentPath string, email string) {
+func (e *Emailer) CreateAndSend(attachmentPath string, email string) error {
 	m := gomail.NewMessage()
 
 	m.SetHeader("From", e.email)
 	m.SetHeader("To", email)
 
-	// m.SetHeader("Subject", "Hello!")
-	// m.SetBody("text/html", "Hello")
 	m.Attach(attachmentPath)
 	d := gomail.NewDialer(e.host, e.port, e.email, e.password)
 
 	if err := d.DialAndSend(m); err != nil {
-		log.DefaultLogger.Error(err.Error())
+		log.DefaultLogger.Error("CreateAndSend: DialAndSend: " + err.Error())
+		return err
 	}
+
+	return nil
 }
 
 func (e *Emailer) BulkCreateAndSend(attachmentPath string, emails []string) {
 	for _, email := range emails {
-		e.CreateAndSend(attachmentPath, email)
+		if err := e.CreateAndSend(attachmentPath, email); err != nil {
+			log.DefaultLogger.Error("BulkCreateAndSend: Could not send to: " + email)
+		}
+
 	}
 }
