f39fd7df2a9832bf162a69de909479b7eb7906dc
josh <griffinjoshua5@gmail.com>
2020-11-12 13:23:25 +1300

    Add remove hello

--- backend/Magefile.go
+++ backend/Magefile.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"fmt"
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
@@ -16,11 +15,6 @@ var cb build.BeforeBuildCallback = func(cfg build.Config) (build.Config, error) 
 }
 
 var err = build.SetBeforeBuildCallback(cb)
-
-// Hello prints a message (shows that you can define custom Mage targets).
-func Hello() {
-	fmt.Println("hello plugin developer!")
-}
 
 // Default configures the default target.
 var Default = build.BuildAll
