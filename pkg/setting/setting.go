d37b0b4297132d691ebd665c9fab31aca3180a78
Sworup Shakya <sworup@users.noreply.github.com>
2022-04-26 12:58:28 +0545

    Adjust errors during setting retrival

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -2,10 +2,12 @@ package setting
 
 import (
 	"context"
+	"excel-report-email-scheduler/pkg/ereserror"
+	"runtime"
 
 	"github.com/bitly/go-simplejson"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
+	"github.com/pkg/errors"
 )
 
 func SettingsFields() string {
@@ -20,11 +22,12 @@ func SettingsFields() string {
 }
 
 func NewSettings(ctx context.Context) (*Settings, error) {
+	frame := trace()
 	pluginCxt := httpadapter.PluginConfigFromContext(ctx)
 
 	jsonData, err := simplejson.NewJson(pluginCxt.AppInstanceSettings.JSONData)
 	if err != nil {
-		log.DefaultLogger.Error("simplejson.NewJson:" + err.Error())
+		err = ereserror.New(500, errors.Wrap(err, frame.Function), "Could not retrive setting")
 		return nil, err
 	}
 
@@ -35,7 +38,6 @@ func NewSettings(ctx context.Context) (*Settings, error) {
 	senderEmailHost := jsonData.Get("senderEmailHost").MustString()
 	datasourceID := jsonData.Get("datasourceID").MustInt()
 
-	log.DefaultLogger.Debug("Secure password: ", pluginCxt.AppInstanceSettings.DecryptedSecureJSONData["grafanaPassword"])
 	var grafanaPassword string
 	if securePassword, exists := pluginCxt.AppInstanceSettings.DecryptedSecureJSONData["grafanaPassword"]; exists {
 		grafanaPassword = securePassword
@@ -44,7 +46,6 @@ func NewSettings(ctx context.Context) (*Settings, error) {
 		grafanaPassword = jsonData.Get("grafanaPassword").MustString()
 	}
 
-	log.DefaultLogger.Debug("Secure password: ", pluginCxt.AppInstanceSettings.DecryptedSecureJSONData["senderEmailPassword"])
 	var emailPassword string
 	if secureEmailPassword, exists := pluginCxt.AppInstanceSettings.DecryptedSecureJSONData["senderEmailPassword"]; exists {
 		emailPassword = secureEmailPassword
@@ -54,4 +55,12 @@ func NewSettings(ctx context.Context) (*Settings, error) {
 	}
 
 	return &Settings{GrafanaUsername: grafanaUsername, GrafanaPassword: grafanaPassword, GrafanaURL: grafanaURL, SenderEmailAddress: senderEmailAddress, SenderEmailPort: senderEmailPort, SenderEmailPassword: emailPassword, SenderEmailHost: senderEmailHost, DatasourceID: datasourceID}, nil
+}
+
+func trace() *runtime.Frame {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return &frame
 }
